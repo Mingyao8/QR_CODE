a9af7ad014290066acaaf7303d3072e4d23c6ba4
MingYao <96293549+Mingyao8@users.noreply.github.com>
2023-05-13 20:36:40 +0800

    Add files via upload

---
+++ QR_CODE.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 20:27:34 2023
+
+@author: Ming Yao
+"""
+import qrcode
+# Link for website
+input_data = "https://www.adultswim.com/etcetera/elastic-man/app.html"
+#Creating an instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
